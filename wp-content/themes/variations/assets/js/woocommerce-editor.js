b02f42c5727c0f6a93f8b609576d83a71e51117d
Kulimák Máté <kulimakmate@turr.hu>
2024-04-16 14:11:30 +0200

    commit

---
+++ wp-content/themes/variations/assets/js/woocommerce-editor.js
@@ -0,0 +1,60 @@
+/**
+ * This file contains JavaScript for the WooCommerce editor part
+ *
+ */
+
+/** woocommerce/price-filter style */
+wp.blocks.registerBlockStyle('woocommerce/price-filter',
+    {
+        name: 'greeni-style',
+        label: 'Greeni Style',
+    }
+);
+
+/** core/post-template style */
+wp.blocks.registerBlockStyle('core/post-template',
+    {
+        name: 'greeni-style',
+        label: 'Greeni Style',
+    }
+);
+
+/** woocommerce/product-categories style */
+wp.blocks.registerBlockStyle('woocommerce/product-categories',
+    {
+        name: 'greeni-style',
+        label: 'Greeni Style',
+    }
+);
+
+/** woocommerce/product-image-gallery style */
+wp.blocks.registerBlockStyle('woocommerce/product-image-gallery',
+    {
+        name: 'greeni-style',
+        label: 'Greeni Style',
+    }
+);
+
+/** woocommerce/add-to-cart-form style */
+wp.blocks.registerBlockStyle('woocommerce/add-to-cart-form',
+    {
+        name: 'greeni-style',
+        label: 'Greeni Style',
+    }
+);
+
+/** woocommerce/product-meta style */
+wp.blocks.registerBlockStyle('woocommerce/product-meta',
+    {
+        name: 'greeni-style',
+        label: 'Greeni Style',
+    }
+);
+
+/** woocommerce/product-details style */
+wp.blocks.registerBlockStyle('woocommerce/product-details',
+    {
+        name: 'greeni-style',
+        label: 'Greeni Style',
+    }
+);
